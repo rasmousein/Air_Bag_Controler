94c7c9218ebbd11f3e20b1e67e2052de42cd66ec
rasmousein <rasmousein@hotmail.com>
2022-02-23 15:51:49 -0400

    first comit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
